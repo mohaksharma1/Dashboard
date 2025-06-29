b6ba358db1ff620a5f9c607ae31ef5f543060893
mohaksharma1 <mohaksharma1@gmail.com>
2025-06-29 16:10:19 +0530

    updater change

--- takeOrder_runner.bat
+++ takeOrder_runner.bat
@@ -1,1 +1,11 @@
-python "takeOrder.py"
+@echo off
+:run_script
+python "src/takeOrder.py"
+IF %ERRORLEVEL% EQU 1 (
+    echo Script returned code 1, rerunning...
+    goto run_script
+) ELSE (
+    echo Script exited with code %ERRORLEVEL%, stopping.
+)
+
+
--- updateMe.py
+++ updateMe.py
@@ -1,18 +1,39 @@
-import os
+import os, sys, shutil
 from git import Repo
 
 def clone_repo(repo_url, destination_folder=None):
     try:
         if destination_folder is None:
+<<<<<<< Updated upstream
             destination_folder = os.path.join(os.getcwd(), repo_url.rstrip('/').split('/')[-1].replace('.git', ''))
 
+=======
+            destination_folder = os.path.join(os.getcwd(),"")
+>>>>>>> Stashed changes
         print(f"Cloning {repo_url} into {destination_folder}...")
         Repo.clone_from(repo_url, destination_folder)
         print("Repository cloned successfully!")
     except Exception as e:
         print(f"Error cloning repository: {e}")
 
+
+
+
+
+
 # Example usage
 if __name__ == "__main__":
+    print("updating...")
     github_url = "https://github.com/mohaksharma1/Dashboard.git"  # Replace with your desired repo
+<<<<<<< Updated upstream
     clone_repo(github_url)
+=======
+    os.system('copy "./src/orders.db" "C:\\Users\\mohak\\Documents"')
+    clone_repo(github_url)
+    shutil.rmtree("src")
+    os.mkdir("src")
+    shutil.copytree(os.path.join(os.getcwd(),"Dashboard\\src"), os.path.join(os.getcwd(),"src"))
+    os.system(f'copy  "C:\\Users\\mohak\\Documents\\orders.db" {os.path.join(os.getcwd(),"src")}')
+    print("update complete")
+    sys.exit(1)
+>>>>>>> Stashed changes
