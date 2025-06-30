9456d65d5f66f57661b405cf26e73a36c937d8dd
mohaksharma1 <mohaksharma1@gmail.com>
2025-06-30 22:18:49 +0530

    Update takeOrder.py

--- src/takeOrder.py
+++ src/takeOrder.py
@@ -1807,6 +1807,7 @@ def testHtml():
         finally:
             python = sys.executable
             os.execl(python, python, *sys.argv)
+            sys.exit(1)
     return """<!DOCTYPE html>
 <html>
    <head>
